d524db01a645ddf9605e7c6e1c605f4696625f4b
Emma Litvin <guslitvem@student.gu.se>
2023-01-27 00:04:34 +0100

    #4 Add comments to the code

--- wp1/bin2Hexa.c
+++ wp1/bin2Hexa.c
@@ -1,65 +1,94 @@
+// (C) Emma Litvin, Nicole Quinstedt. Group: 11 (2023)
+// Work package 1
+// Exercise 4 part 2
+// Submission code: XXXX
+
+/*------------------------------------------------------------------------------------------
+Exercise 4:
+ The program accepts binary number, then converts it to hexadecimal value.
+ Program execution includes following cases:
+ - checks if the string of arguments contains the correct digits,
+ - is able to provide help if the user provides parameter '-h',
+ - outputs an error message if the conversion was not successful, and
+ - returns 2 if the conversion is unsuccessful.
+
+ If running files dec2bin and bin2Hexa in a pipeline, decimal number for conversion should
+ be passes as program argument.
+-------------------------------------------------------------------------------------------*/
+
+// Include section
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-/*
-  dec2bin.c and bin2Hexa.c needs to be have different executions fields. 
-  
-*/
-
+// Function that converts binary number to hexadecimal value
 void convert(char* input){
+    // Variables declaration
+    int decimal = 0,    // Starting value for decimal
+        remainder;      // Variable for remainder
+    int i = 1;          // Value of each step in binary sequence
+
+    // Convert input into integer
     int binary = atoi(input);
-    int decimal = 0, remainder;
-    int i = 1;
-// convert to decimal
+
+    // Convert to decimal if provided input is not 0
     while (binary != 0) {
+        // Get the remained after division by 10
         remainder = binary % 10;
+        // Incrementing value of decimal
         decimal += remainder * i;
+        // Double the value of each step in binary sequence since they are connected by 2^n
         i = i * 2;
+        // Get the whole value after division by 10
         binary = binary / 10;
     }
-// print in hexadecimal	
-    printf("%d\n",decimal);
-    if(decimal > 15){
+    // Print hexadecimal value
+    printf("%d\n", decimal);
+    if(decimal > 15) {   // Case if hexadecimal value is bigger than 15
       printf("%X", decimal); 
-    }else{
+    } else {   // Case if hexadecimal value is equals or less than 15
       printf("0%X", decimal); 
     }
 }
 
+// Main program section
 int main(int argc, char* argv[]) {
- 
-  if(argc < 2){  // if no argument provided check stdin from pipeline  
-    int buffer = getc(stdin); // read stdin
-	
-    if(buffer == EOF){ // if getc fails and no arguments are
+    // Variables declaration
+    char input[11];     // Array to store 12 character values
+    int j = 0;          // Index of element in the array
+
+  // Case if no argument provided, check stdin from pipeline
+  if(argc < 2) {
+    int buffer = getc(stdin); // Read stdin
+	// Prints message if getc() fails and no arguments provided
+    if(buffer == EOF){
       printf("No arguments provided. Use '-h' for help.\n");
-      return 2;
+      return 2;   // Exits with code 2 according to specifications
     }
-
-    char input[11];
-    int j=0; 
-
-    while(buffer != EOF && buffer != '\n'){
-      if( j > 11){ 
+    //
+    while(buffer != EOF && buffer != '\n') {
+      // Print message if given value is bigger than the size of array input
+      if(j > 11) {
         printf("The input is out of range.");
         return 1;
       }
-        input[j] = (char)buffer;
-        buffer = getc(stdin);
-        j++;
+      // Add read values from getc() to the array converting them to char
+      input[j] = (char)buffer;
+      // Read values from stdin
+      buffer = getc(stdin);
+      // Increment array index
+      j++;
     }
-
+    // Add '\0' to the string to mark its end
     input[j]= '\0';
-    printf("%s\n", input);
+    // Convert given input
     convert(input);
-
-  }else if(strcmp(argv[1], "-h") == 0) {
+  } // Prints message if program argument is -h
+  else if(strcmp(argv[1], "-h") == 0) {
     printf("Enter a decimal number to convert to binary. The decimal should not be 0.\n");
     return 1;
-    
-  }else{
+  } else { // Convert given program argument
     convert(argv[1]);
   }
-  return 0;
+  return 0;  // End of the program
 } 
