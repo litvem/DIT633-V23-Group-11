fbfaa804f05e800ed5f40d5d0dc08fa5fd1ba4a0
Emma Litvin <guslitvem@student.gu.se>
2023-01-27 22:39:21 +0100

    #12 Task definition

---
+++ wp2/exercise_1.c
@@ -0,0 +1,22 @@
+// (C) Emma Litvin, Nicole Quinstedt. Group: 11 (2023)
+// Work package 2
+// Exercise 1
+// Submission code: XXXX
+
+/*------------------------------------------------------------------------------------------
+References:
+ -
+-------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------
+Exercise 1: The program asks for robot's starting position (coordinates x and y ranging
+ between 1 and 99) and a string of characters 'm' and 't,
+ where m - tells robot to move one step in current direction,
+       t - tells robot to turn 90 degrees clockwise.
+ The program performs instructions from the strings provided by the user one by one.
+ When all instructions have been executed, the robot stops and the program prints out
+ robot's new current position.
+ Then the program asks for a new starting position and string of characters.
+ The process repeats until the program encounters a dedicated end character 'x'.
+-------------------------------------------------------------------------------------------*/
+
