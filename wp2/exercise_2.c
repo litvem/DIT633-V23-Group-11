dd23e33e1a2c705fbb188d3da2d32ece16061dc6
Emma Litvin <guslitvem@student.gu.se>
2023-01-29 12:31:42 +0100

    #13 Task definition

---
+++ wp2/exercise_2.c
@@ -0,0 +1,8 @@
+// (C) Emma Litvin, Nicole Quinstedt. Group: 11 (2023)
+// Work package 2
+// Exercise 2
+// Submission code: XXXX
+
+/*------------------------------------------------------------------------------------------
+Exercise 2: The program generates random numbers and adds the to a linked list.
+-------------------------------------------------------------------------------------------*/
