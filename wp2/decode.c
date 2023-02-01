8582cd0f236439bc849666ff401ba32bb6db82f8
Nicole Quinstedt <90027419+Quinstedt@users.noreply.github.com>
2023-02-01 21:31:39 +0100

    updateting comments

--- wp2/decode.c
+++ wp2/decode.c
@@ -31,8 +31,8 @@ void print_decoded(unsigned char byte);
 int main(int argc , char** argv ){ 
     // Case if number of provided argument equals to 2
     if(argc == 2) {
-        // Assign values of arguments to two integers accordingly
-        int first4bits = argv[1][0];
+        // Assign values of arguments to two integers accordingly to the ASCII table
+        int first4bits = argv[1][0]; // if it is a number the char value is returned
         int last4bits = argv[1][1];
 
         // Check of argument values using check_input() function
@@ -61,15 +61,15 @@ int main(int argc , char** argv ){
 // Definition of function to check the format of the input
 int check_input(int input) {
     // Case if input value is greater than or equal to 48 and less than 57
-    if(input >= 48 && input < 57) {
+    if(input >= 48 && input < 57) { // check if the value is the char value for a number
         // Reduce value of input by 48
         return input -= 48;
     } // Case if input value is greater than or equal to 65 and less than 71
-    else if(input >= 65 && input < 71) {
+    else if(input >= 65 && input < 71) { // case when the input is a capital letter
         // Reduce value of input by 55
         return input -= 55;
     } // Case if input value is greater than or equal to 97 and less than 103
-    else if(input >= 97 && input < 103) {
+    else if(input >= 97 && input < 103) { // case when the input is a lower case letter
         // Reduce value of input by 87
         return input -= 87;
     } // Notify user if provided hexadecimal number was incorrect
@@ -89,19 +89,23 @@ void print_decoded(unsigned char byte){
     int brake2;
 
     // Get value of engine using bit shifting
-    engine = byte >> 7; 
+    engine = byte >> 7;
+    // removing the engine value (1 bit) by bit shifting 1 step to the left
     byte = byte << 1;
 
     // Get value of gear using bit shifting
     gear = byte >> 5;
+    // removing the gear value (3 bits) from the byte by bit shifting 3 step to the left
     byte = byte << 3;
 
     // Get value of key using bit shifting
     key = byte >> 6;
+    // removing the key values ( 2 bits) by bit shifting 2 steps to the left
     byte = byte << 2;
 
     // Get value of brake1 using bit shifting
     brake1 = byte >> 7;
+    // removing the brake1 value (1 bit) by bit shifting 1 step to the left
     byte = byte << 1;
 
     // Get value of brake2 using bit shifting
