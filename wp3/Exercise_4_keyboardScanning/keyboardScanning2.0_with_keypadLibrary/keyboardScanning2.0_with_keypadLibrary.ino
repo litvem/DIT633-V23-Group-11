17b627aa1cf98d018f0ad48c5b06688c1809fced
Quinstedt <90027419+Quinstedt@users.noreply.github.com>
2023-02-05 13:23:44 +0100

    #25 KeyboardScanning 2.0 with KeyPad Library Sending the correct code for that exercise.

--- wp3/Exercise_4_keyboardScanning/keyboardScanning2.0_with_keypadLibrary/keyboardScanning2.0_with_keypadLibrary.ino
+++ wp3/Exercise_4_keyboardScanning/keyboardScanning2.0_with_keypadLibrary/keyboardScanning2.0_with_keypadLibrary.ino
@@ -1,24 +1,27 @@
-check /*
- * ALternative solution using bits
- * https://www.youtube.com/watch?v=ycV1cP_anvI
- * 
- * My tinkercad: https://www.tinkercad.com/things/aGwsjBBZzrz?sharecode=qyrUYI9xzcoOuT34ELfh3gIJCnXzNnfORbNOZoVgtII
- */
+ /* WITH keypad.h
+ * Reference: https://playground.arduino.cc/Code/Keypad/
+*/
 
-#define cols 4
-#define rows 4 
+#include <Keypad.h>
 
-const int colsPin[cols]={7,6,5,4};
-const int rowsPin[rows]={11,10,9,8};
+const byte rows = 4; // number of rows in the keyboard
+const byte cols = 4; // number of columns in the keyboard
+
+byte colsPins[cols]={7,6,5,4}; // array of pins for each column
+byte rowsPins[rows]={11,10,9,8}; // array of pings for each row
 
 // Two dimentional char array representation of the keyboard
 // use for the printing of the keyboard values that have been pressed 
-char keyMatrix[rows][cols]={
+char keyMatrix[rows][cols] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
+
+
+//Instantiates a Keypad object that uses pins 11, 10, 9, 8 as row pins, and 7, 6, 5, 4 as column pins.
+Keypad keypad = Keypad (makeKeymap(keyMatrix), rowsPins, colsPins, rows, cols);
 
 // setup function to establish serial communication
 void setup(){
@@ -26,35 +29,12 @@ void setup(){
   // The Baud rate is set to 9600, meaning that the serail port is capable of transferring a maximum of 9600 bits per second.
 }
 
-// loop function
-void loop() {
-	// loops the readKey function which prints the char pressed in the keyboard
-  readKey();
-}
 
-void readKey(){
-// Loop through all rows 
-  for (int r=0; r<rows; r++) {
-  /*
-    pinMode function configure a specific pin to behave either as an input or output, in this case is set to output
-    (documnetation: https://www.arduino.cc/reference/en/language/functions/digital-io/pinmode/) 
-   */
-    pinMode(rowsPin[r], OUTPUT); // set the current row as OUTPUT
+void loop(){
+  char key = keypad.getKey(); // Store the key that is pressed in a char called key
 
-    // Loop through all colums
-    for(int c=0; c<cols; c++){
-      
-      //digitalRead() reads the value of a pin as either LOW or HIGH.
-      if (digitalRead(colsPin[c]) == LOW) { // Check if the pin is LOW, which means that it has been pressed.
-        
-        // Prints the pressed key which is the character in the current row (r) and column (c) from the keyboard matrix.
-        Serial.println(keyMatrix[r][c]); 
-        
-        // delay by a second
-        delay(1000);
-      }
-    }
-    // Reset the row as INPUT mode 
-    pinMode(rowsPin[r], INPUT);
+  if (key != NO_KEY){ // Condition that check if the key is not the empty
+    Serial.println(key); // Print the pressed key
   }
 }
+
