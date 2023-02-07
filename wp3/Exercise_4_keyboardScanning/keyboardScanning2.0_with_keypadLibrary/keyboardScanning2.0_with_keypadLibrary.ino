7dc7ea6102fb069bef4432c6e1ee1e77203b99e3
Emma Litvin <guslitvem@student.gu.se>
2023-02-07 21:41:04 +0100

    #25 Clean code comments

--- wp3/Exercise_4_keyboardScanning/keyboardScanning2.0_with_keypadLibrary/keyboardScanning2.0_with_keypadLibrary.ino
+++ wp3/Exercise_4_keyboardScanning/keyboardScanning2.0_with_keypadLibrary/keyboardScanning2.0_with_keypadLibrary.ino
@@ -1,17 +1,32 @@
- /* WITH keypad.h
- * Reference: https://playground.arduino.cc/Code/Keypad/
-*/
+// (C) Emma Litvin, Nicole Quinstedt. Group: 11 (2023)
+// Work package 3
+// Exercise 4
+// Submission code: XXXX
 
+/*--------------------------------------------------------------------------------------------
+Exercise 4: The program reads the keyboard input and prints out the key number in the serial 
+  monitor if a key is pressed. If no key is pressed, nothing is printed out.
+  The solutions is created using Keypad.h library.
+---------------------------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------------------------
+  Sources:
+  - Keypad.h library documentation:
+    https://playground.arduino.cc/Code/Keypad/
+----------------------------------------------------------------------------------------------*/
+
+// Include section
 #include <Keypad.h>
 
-const byte rows = 4; // number of rows in the keyboard
-const byte cols = 4; // number of columns in the keyboard
+// Define section
+const byte rows = 4;                            // Number of rows in the keyboard
+const byte cols = 4;                            // Number of columns in the keyboard
 
-byte colsPins[cols]={7,6,5,4}; // array of pins for each column
-byte rowsPins[rows]={11,10,9,8}; // array of pings for each row
+byte colsPins[cols] = {7, 6, 5, 4};             // Array of pins for each column
+byte rowsPins[rows] = {11, 10, 9, 8};           // Array of pins for each row
 
-// Two dimentional char array representation of the keyboard
-// use for the printing of the keyboard values that have been pressed 
+/* Two dimentional char array representation of the keyboard used for printing of the keyboard values that have been pressed. 
+   The ASCII values used for numbers. */
 char keyMatrix[rows][cols] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
@@ -20,21 +35,26 @@ char keyMatrix[rows][cols] = {
 };
 
 
-//Instantiates a Keypad object that uses pins 11, 10, 9, 8 as row pins, and 7, 6, 5, 4 as column pins.
+// Instantiate a Keypad object that uses pins 11, 10, 9, 8 as row pins, and 7, 6, 5, 4 as column pins
 Keypad keypad = Keypad (makeKeymap(keyMatrix), rowsPins, colsPins, rows, cols);
 
-// setup function to establish serial communication
-void setup(){
-  Serial.begin(9600); // Start the serial communication between two devices, through the USB connection
-  // The Baud rate is set to 9600, meaning that the serail port is capable of transferring a maximum of 9600 bits per second.
+// Function that will run the code only once
+void setup() 
+{
+  Serial.begin(9600);                           // Start serial communication between two devices, through the USB connection
+                                                // The Baud rate is set to 9600, meaning that the serial port is capable of transferring a maximum of 9600 bits per second.
 }
 
+// Function that runs the code repeatedly
+void loop()
+{
+  // Store the key that is pressed in a variable key
+  char key = keypad.getKey(); 
 
-void loop(){
-  char key = keypad.getKey(); // Store the key that is pressed in a char called key
-
-  if (key != NO_KEY){ // Condition that check if the key is not the empty
-    Serial.println(key); // Print the pressed key
+  // Condition that check if the key is not the empty
+  if (key != NO_KEY)
+  { 
+    // Print the pressed key
+    Serial.println(key); 
   }
 }
-
